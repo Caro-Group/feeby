babd922aac35e19e9f6f52d6e8528a52f22e2fb6
Triloworld <2671726+Triloworld@users.noreply.github.com>
2022-01-21 12:55:56 +0100

    [FEEB-350] Sliders

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1476,3 +1476,10 @@ $(document).ready(function () {
 $(document).ready(function () {
 	$('#checkout [name="postcode"]').mask("99-999");
 });
+
+$(document).ready(function () {
+	const swiper = new Swiper('[data-swiper-carousel]', {
+		speed: 400,
+		spaceBetween: 10,
+	});
+});
