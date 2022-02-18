9415df0de091faed8da9247d4aa173ce60abce53
Kamil <kamil@webo.agency>
2022-02-18 09:39:32 +0100

    popup delete, animation separate file

--- modules/leofeature/views/js/leofeature_cart.js
+++ modules/leofeature/views/js/leofeature_cart.js
@@ -63,10 +63,6 @@ $(document).ready(function(){
 	// $('.blockcart.cart-preview').removeAttr('refresh-url');
 	// $('.blockcart.cart-preview').wrap('<div class="leo-blockcart-wapper"></div>');
 
-	$('.product-add-to-cart button').click(function(){
-		$(this).addClass('adding');
-	});
-
 	prestashop.on('updateCart', function (event) {
 		
 		// console.log('aaa');
@@ -102,7 +98,6 @@ $(document).ready(function(){
 					}
 				},
 				error: function (XMLHttpRequest, textStatus, errorThrown) {
-					$('.product-add-to-cart button').removeClass('adding');
 					console.log("TECHNICAL ERROR: \n\nDetails:\nError thrown: " + XMLHttpRequest + "\n" + 'Text status: ' + textStatus);
 				}
 			});
@@ -110,12 +105,7 @@ $(document).ready(function(){
 		//DONGND:: show notification if add cart success
 		if (event.reason.linkAction == 'add-to-cart' && event.resp.success)
 		{		
-			// console.log($('.leo-bt-cart.active'));
-			$('.product-add-to-cart button').switchClass('adding','added');
-			setTimeout(function () {
-				$('.product-add-to-cart button').removeClass('added');						
-			},2000);		
-			
+			// console.log($('.leo-bt-cart.active'));			
 			//DONGND:: run fly cart
 			if (typeof enable_flycart_effect != 'undefined' && enable_flycart_effect)
 			{
@@ -131,23 +121,23 @@ $(document).ready(function(){
 				}
 			}
 			
-			if ($('.leo-notification').length && typeof enable_notification != 'undefined' && enable_notification)
-			{
-				//DONGND:: show notification
-				var id_product = event.resp.id_product;
-				var product_name = false;
-				$.each(event.resp.cart.products, function(key, value){
-					// console.log(value);
-					// console.log(value.id_product);			
-					// console.log(value.name);
-					if (id_product == value.id_product)
-					{
-						product_name = value.name;
-						return false;
-					}
-				})				
-				showLeoNotification('success', 'add', product_name);				
-			}
+			// if ($('.leo-notification').length && typeof enable_notification != 'undefined' && enable_notification)
+			// {
+			// 	//DONGND:: show notification
+			// 	var id_product = event.resp.id_product;
+			// 	var product_name = false;
+			// 	$.each(event.resp.cart.products, function(key, value){
+			// 		// console.log(value);
+			// 		// console.log(value.id_product);			
+			// 		// console.log(value.name);
+			// 		if (id_product == value.id_product)
+			// 		{
+			// 			product_name = value.name;
+			// 			return false;
+			// 		}
+			// 	})				
+			// 	showLeoNotification('success', 'add', product_name);				
+			// }
 		}
 		
 		//DONGND:: hide cart item has been deleted from page cart
@@ -211,23 +201,23 @@ $(document).ready(function(){
 		// }
 		
 		// console.log('test1');
-		if (typeof show_popup != 'undefined' && show_popup)
-		{
-			// console.log('test');
-			check_active_modal_cart = setInterval(function(){
+		// if (typeof show_popup != 'undefined' && show_popup)
+		// {
+		// 	// console.log('test');
+		// 	check_active_modal_cart = setInterval(function(){
 				
-				if ($('.leo-bt-cart.active').length && $('#blockcart-modal').length && $('#blockcart-modal').hasClass('modal fade in'))
-				{
+		// 		if ($('.leo-bt-cart.active').length && $('#blockcart-modal').length && $('#blockcart-modal').hasClass('modal fade in'))
+		// 		{
 					
-					$('.leo-bt-cart.active').find('.leo-bt-cart-content').fadeIn('fast');
-					$('.leo-bt-cart.active').find('.leo-loading').hide();
-					$('.leo-bt-cart.active').removeClass('active reset');
+		// 			$('.leo-bt-cart.active').find('.leo-bt-cart-content').fadeIn('fast');
+		// 			$('.leo-bt-cart.active').find('.leo-loading').hide();
+		// 			$('.leo-bt-cart.active').removeClass('active reset');
 					
-					clearInterval(check_active_modal_cart);
-				}
+		// 			clearInterval(check_active_modal_cart);
+		// 		}
 				
-			}, 200);
-		}
+		// 	}, 200);
+		// }
 		
 		if (typeof show_popup != 'undefined' && !show_popup)
 		{
