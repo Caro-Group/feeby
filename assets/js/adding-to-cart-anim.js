85b5dffd80d7e7aedb3d45c77cf180de0bcb38e1
Kamil <kamil@webo.agency>
2022-02-19 14:56:19 +0100

    btn selector fix

--- assets/js/adding-to-cart-anim.js
+++ assets/js/adding-to-cart-anim.js
@@ -1,33 +1,33 @@
 $(document).ready(function(){
 
     var waiting=0;
-    $('body').on('click','.product-add-to-cart button',function(){
-        $('.product-add-to-cart button').addClass('adding');
+    $('body').on('click','.product-add-to-cart .add button',function(){
+        $('.product-add-to-cart .add button').addClass('adding');
         waiting++;
     });
     
-    prestashop.on('updateCart', function () {
-        waiting--;
-        if (waiting == 0 ) {
-            $('body').find('.product-add-to-cart button').switchClass('adding','added');
-            setTimeout(function () {
-                $('body').find('.product-add-to-cart button').removeClass('added');						
-            },2000);		
+    prestashop.on('updateCart', function (event) {
+        if (event.reason.linkAction == "add-to-cart") {            
+            waiting--;
+            if (waiting == 0 ) {
+                $('body').find('.product-add-to-cart .add button').switchClass('adding','added');
+                setTimeout(function () {
+                    $('body').find('.product-add-to-cart .add button').removeClass('added');						
+                },1500);		
+            }
         }
     })
     
     prestashop.on('handleError',function (event) {
-        console.log(event.eventType)
         if (event.eventType == "updateProductInCart") {
-            $('body').find('.product-add-to-cart button').switchClass('adding','failed');
+            $('body').find('.product-add-to-cart .add button').switchClass('adding','failed');
             setTimeout(function () {
-                $('body').find('.product-add-to-cart button').removeClass('failed');						
+                $('body').find('.product-add-to-cart .add button').removeClass('failed');						
             },3000);
         }
         waiting = 0;
     })
 
-    // HIDING POPUP
     // HIDING POPUP
     $('body').on('shown.bs.modal', '#blockcart-modal',function(){
       
