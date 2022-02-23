a5c62f0b15b74ea8e3d4364faed8139ca7e8f045
Triloworld <2671726+Triloworld@users.noreply.github.com>
2022-02-23 12:19:12 +0100

    Squashed commit of the following:
    commit 5c14dd363dd98fe72f4b0b6ed601d8eaae81ec03
    Author: Kamil <kamil@webo.agency>
    Date:   Fri Feb 18 15:51:59 2022 +0100
    
        deleete backdrop blur
    
    commit 7c39a049db600ca024e9c140e85377146a631d4e
    Author: Kamil <kamil@webo.agency>
    Date:   Fri Feb 18 15:41:58 2022 +0100
    
        loader styles fix
    
    commit 2e9dd5aa7de4d29fa8421c66952cc63a9c2e7115
    Author: Kamil <kamil@webo.agency>
    Date:   Fri Feb 18 15:22:33 2022 +0100
    
        z index
    
    commit a5ce3457dd2ac03adc94aa6e0038b16ad1b1cd7e
    Author: Kamil <kamil@webo.agency>
    Date:   Fri Feb 18 15:18:47 2022 +0100
    
        loader
    
    commit 7f54aa204334ca7c308a9f431fe6f1cd8c6834b6
    Author: Kamil <kamil@webo.agency>
    Date:   Fri Feb 18 15:18:39 2022 +0100
    
        loader

---
+++ assets/js/loader.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const body = document.querySelector('body');
+    
+    window.addEventListener('beforeunload',function(){
+        body.insertAdjacentHTML('afterbegin','     <div class="loader bg-main-dark bg-opacity-50 fixed left-0 top-0 w-full h-full "><div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 "><span class="w-[75px] h-[75px] block border-[2px] border-white border-opacity-60 border-t-main border-solid rounded-full animate-spin " ></span></div></div> ')
+        
+    })
+
+});
