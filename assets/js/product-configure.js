de850c84e07d2ab43fa154d04abb3bb25e45e05b
Kamil <kamil@webo.agency>
2022-03-01 21:30:21 +0100

    remove unnecessary if's and comments

--- assets/js/product-configure.js
+++ assets/js/product-configure.js
@@ -60,86 +60,6 @@ $(document).ready(function () {
             }
         );
     }
-      
-      
-    
-    
-    // var firstIndex = undefined;
-    // var lastIndex = undefined;
-    // var tempIndex = undefined;
-
-    // var partsArray = $('[data-product-configurable="swiper"] [data-product-configurable]').each(function () {
-
-    //     $(this).mouseenter(function(){
-    //         if(firstIndex!= undefined && lastIndex == undefined){
-    //             tempIndex = $(this).attr('data-product-configurable');
-    //             if (firstIndex<tempIndex) {
-    //                 for( i = firstIndex ; i < tempIndex ; i++ ){
-    //                     $(partsArray[i-1]).addClass('selected');
-    //                 }
-    //                 for( i = tempIndex ; i < partsArray.length ; i++ ){
-    //                     $(partsArray[i-1]).removeClass('selected');
-    //                 }
-    //             } else {
-    //                 for( i = firstIndex ; i >= 0 ; i-- ){
-    //                     $(partsArray[i-1]).addClass('selected');
-    //                 }
-    //                 for( i = 0 ; i < tempIndex ; i++ ){
-    //                     $(partsArray[i-1]).removeClass('selected');
-    //                 }
-                    
-    //             }
-    //         }
-    //     })
-
-    //     $(this).mouseleave(function(){
-    //         if(firstIndex!=undefined){
-    //             for( i = 0 ; i < partsArray.length ; i++ ){
-    //                 if(i!=firstIndex-1 && lastIndex==undefined){
-    //                     $(partsArray[i]).removeClass('selected');
-    //                 }
-    //             }
-    //         }
-    //     })
-
-	// 	$(this).click(() => {
-
-    //         if(firstIndex==undefined){
-    //             $(this).addClass('selected');
-    //             firstIndex =  $(this).attr('data-product-configurable');
-    //         }else if( lastIndex==undefined){
-    //             if(firstIndex!=$(this).attr('data-product-configurable')){
-    //                 $(this).addClass('selected');
-    //                 lastIndex =  $(this).attr('data-product-configurable');
-    //             }else{
-    //                 firstIndex = undefined;
-    //                 lastIndex= undefined;
-    //                 $(this).removeClass('selected');
-
-    //             }
-    //         }else if(firstIndex!= undefined && lastIndex!= undefined){
-    //             firstIndex =  $(this).attr('data-product-configurable');
-    //             lastIndex = undefined;
-    //             for( i = 0 ; i < partsArray.length ; i++ ){
-    //                 if(i!=firstIndex-1){
-    //                     $(partsArray[i]).removeClass('selected');
-    //                 }
-    //             }   
-    //         }
-    //     })
-	// })
-
-    // $('body').on("click", function(e){ 
-
-    //         if($(e.target).has('[data-product-configurable="swiper"] [data-product-configurable].swiper-slide').length == 1 ){
-    //             for( i = 0 ; i < partsArray.length ; i++ ){
-    //                 $(partsArray[i]).removeClass('selected');
-    //                 firstIndex = undefined;
-    //                 lastIndex = undefined;
-    //             } 
-    //         }
-    // });
-    
 });
 
 
@@ -169,47 +89,39 @@ function productConfigurableSelect(productId){
         configurableSelected[index].selected = true;
     }
     else{
-        if(configurableSelectedTemp.length > 0){
-            if (configurableSelected[index].selected == false) {
-                
-                // check surrounding from right 
-                if (index < configurableSelectedTemp[0].id - 1) {
-                    for(i = index ; i < configurableSelectedTemp[0].id -1 ; i++){
-                        configurableSelected[i].selected = true;
-                    }                
-                }
-
-                // check surrounding from left 
-                else if (index > configurableSelectedTemp[0].id - 1) {
-                    for(i = index ; i > configurableSelectedTemp[configurableSelectedTemp.length - 1].id -1 ; i--){
-                        configurableSelected[i].selected = true;
-                    }                
-                }
+        if (configurableSelected[index].selected == false) {          
+            // check surrounding from right 
+            if (index < configurableSelectedTemp[0].id - 1) {
+                for(i = index ; i < configurableSelectedTemp[0].id -1 ; i++){
+                    configurableSelected[i].selected = true;
+                }                
             }
-
-            else if(configurableSelectedTemp.length !== 1 && configurableSelected[index].selected == true){          
-                
-                // disabling one when clicked on selected edge
-                if(configurableSelected[index].selected == true && ( configurableSelected[index].id == configurableSelectedTemp[0].id || configurableSelectedTemp[configurableSelectedTemp.length - 1].id == configurableSelected[index].id ) ){
-                    configurableSelected[index].selected = false;
+            // check surrounding from left 
+            else if (index > configurableSelectedTemp[0].id - 1) {
+                for(i = index ; i > configurableSelectedTemp[configurableSelectedTemp.length - 1].id -1 ; i--){
+                    configurableSelected[i].selected = true;
+                }                
+            }
+        }
+        else if(configurableSelectedTemp.length !== 1 && configurableSelected[index].selected == true){          
+            // disabling one when clicked on selected edge
+            if(configurableSelected[index].selected == true && ( configurableSelected[index].id == configurableSelectedTemp[0].id || 
+                configurableSelectedTemp[configurableSelectedTemp.length - 1].id == configurableSelected[index].id ) ){
+                configurableSelected[index].selected = false;
+            }
+            // disabling to sides from inside of selected group 
+            else{
+                var tempIndex = configurableSelectedTemp.findIndex(product => product.id == productId  )
+                if (tempIndex < Math.trunc(configurableSelectedTemp.length/2 )) {
+                    for(i = index - 1 ; i >= configurableSelectedTemp[0].id - 1 ; i--){
+                        configurableSelected[i].selected = false;
+                    }   
                 }
-
-                // disabling to sides from inside of selected group 
                 else{
-                    var tempIndex = configurableSelectedTemp.findIndex(product => product.id == productId  )
-    
-                    if (tempIndex < Math.trunc(configurableSelectedTemp.length/2 )) {
-                        for(i = index - 1 ; i >= configurableSelectedTemp[0].id - 1 ; i--){
-                            configurableSelected[i].selected = false;
-                        }   
-                    }
-                    else{
-                        for(i = index + 1 ; i < configurableSelectedTemp[configurableSelectedTemp.length - 1].id ; i++){
-                            configurableSelected[i].selected = false;
-                        }   
-     
-                    }
-                }
+                    for(i = index + 1 ; i < configurableSelectedTemp[configurableSelectedTemp.length - 1].id ; i++){
+                        configurableSelected[i].selected = false;
+                    }   
+                }              
             }
         }
     }
