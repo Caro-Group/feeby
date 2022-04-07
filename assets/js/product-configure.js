0ebd586ea4c3b1a6060a7c933ce61bd98da9f4a8
Triloworld <2671726+Triloworld@users.noreply.github.com>
2022-04-07 12:50:53 +0200

    FEEB-530 FEEB-421 Configurator

--- assets/js/product-configure.js
+++ assets/js/product-configure.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
   if (typeof prestashop !== 'undefined') {
     prestashop.on('updatedProduct', function (event) {
       var imageVariant = $(event.product_cover_thumbnails)
-        .find('img.#product_original_img')
+        .find('img#product_original_img')
         .first()
         .attr('src')
 
