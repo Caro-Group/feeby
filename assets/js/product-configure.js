1005d8e4a51fe330aff796ce4df14e2a1df350fd
Triloworld <2671726+Triloworld@users.noreply.github.com>
2022-04-13 17:52:17 +0200

    FEEB-542 Redirect

--- assets/js/product-configure.js
+++ assets/js/product-configure.js
@@ -62,6 +62,7 @@ $(document).ready(function () {
         $('.hidden [data-button-action="add-to-cart"]').trigger('click')
         productConfigurableUnSelectAll()
         productConfigurableSetState()
+        window.location.assign(prestashop.urls.pages.cart)
       }
     }
   )
