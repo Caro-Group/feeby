95355fc571f55273be0c0cb316c7c2a6f6246ada
Triloworld <2671726+Triloworld@users.noreply.github.com>
2022-06-08 18:27:48 +0200

    JS fix

--- modules/leofeature/views/js/jquery.rating.pack.js
+++ modules/leofeature/views/js/jquery.rating.pack.js
@@ -1,59 +1,120 @@
-/**
- * 2007-2017 Leotheme
- *
- * NOTICE OF LICENSE
- *
- * Leo feature for prestashop 1.7: ajax cart, review, compare, wishlist at product list
- *
- * DISCLAIMER
- *
- *  @Module Name: Leo Feature
- *  @author    leotheme <leotheme@gmail.com>
- *  @copyright 2007-2018 Leotheme
- *  @license   http://leotheme.com - prestashop template provider
- */
-/*
- ### jQuery Star Rating Plugin v2.0 - 2008-03-12 ###
- By Diego A, http://www.fyneworks.com, diego@fyneworks.com
- - v2 by Keith Wood, kbwood@virginbroadband.com.au
- 
- Project: http://plugins.jquery.com/project/MultipleFriendlyStarRating
- Website: http://www.fyneworks.com/jquery/star-rating/
-	
-	This is a modified version of the star rating plugin from:
- http://www.phpletter.com/Demo/Jquery-Star-Rating-Plugin/
-*/
-eval(
-  (function (p, a, c, k, e, r) {
-    e = function (c) {
-      return (
-        (c < a ? '' : e(parseInt(c / a))) +
-        ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
-      )
-    }
-    if (!''.replace(/^/, String)) {
-      while (c--) r[e(c)] = k[c] || e(c)
-      k = [
-        function (e) {
-          return r[e]
+if (jQuery)
+  (function ($) {
+    $.fn.rating = function (c) {
+      c = $.extend(
+        {
+          cancel: 'Cancel Rating',
+          cancelValue: '',
+          required: false,
+          readOnly: false,
         },
-      ]
-      e = function () {
-        return '\\w+'
+        c || {},
+      )
+      var d = {}
+      var e = {
+        fill: function (n, a, b) {
+          this.drain(n)
+          $(a)
+            .prevAll('.star')
+            .andSelf()
+            .addClass(b || 'star_hover')
+        },
+        drain: function (n) {
+          $(d[n].valueElem)
+            .siblings('.star')
+            .removeClass('star_on')
+            .removeClass('star_hover')
+        },
+        reset: function (n) {
+          if (!$(d[n].currentElem).is('.cancel')) {
+            $(d[n].currentElem).prevAll('.star').andSelf().addClass('star_on')
+          }
+        },
+        click: function (n, a) {
+          d[n].currentElem = a
+          var b = $(a).children('a').text()
+          $(d[n].valueElem).val(b)
+          e.drain(n)
+          e.reset(n)
+          if (c.callback) c.callback.apply(d[n].valueElem, [b, a])
+        },
       }
-      c = 1
+      this.each(function (i) {
+        var n = this.name
+        if (!d[n])
+          d[n] = {
+            count: 0,
+          }
+        i = d[n].count
+        d[n].count++
+        if (i == 0) {
+          c.readOnly = $(this).attr('disabled') || c.readOnly
+          d[n].valueElem = $(
+            '<input type="hidden" name="' +
+              n +
+              '" value=""' +
+              (c.readOnly ? ' disabled="disabled"' : '') +
+              '>',
+          )
+          $(this).before(d[n].valueElem)
+          if (c.readOnly || c.required) {
+          } else {
+            $(this).before(
+              $(
+                '<div class="cancel"><a title="' +
+                  c.cancel +
+                  '">' +
+                  c.cancelValue +
+                  '</a></div>',
+              )
+                .on('mouseover', function () {
+                  e.drain(n)
+                  $(this).addClass('star_on')
+                })
+                .on('mouseout', function () {
+                  e.reset(n)
+                  $(this).removeClass('star_on')
+                })
+                .click(function () {
+                  e.click(n, this)
+                }),
+            )
+          }
+        }
+        eStar = $(
+          '<div class="star"><a title="' +
+            (this.title || this.value) +
+            '">' +
+            this.value +
+            '</a></div>',
+        )
+        $(this).after(eStar)
+        if (c.readOnly) {
+          $(eStar).addClass('star_readonly')
+        } else {
+          $(eStar)
+            .on('mouseover', function () {
+              e.drain(n)
+              e.fill(n, this)
+            })
+            .on('mouseout', function () {
+              e.drain(n)
+              e.reset(n)
+            })
+            .on('click', function () {
+              e.click(n, this)
+            })
+        }
+        if (this.checked) d[n].currentElem = eStar
+        $(this).remove()
+        if (i + 1 == this.length) e.reset(n)
+      })
+      for (n in d)
+        if (d[n].currentElem) {
+          e.fill(n, d[n].currentElem, 'star_on')
+          $(d[n].valueElem).val($(d[n].currentElem).children('a').text())
+        }
+      return this
     }
-    while (c--)
-      if (k[c]) p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c])
-    return p
-  })(
-    ";4(w)(3($){$.Y.T=3(c){c=$.15({m:'X U',E:'',B:z,8:z},c||{});o d={};o e={t:3(n,a,b){2.6(n);$(a).D('.j').A().l(b||'x')},6:3(n){$(d[n].7).O('.j').u('9').u('x')},h:3(n){4(!$(d[n].5).Z('.m')){$(d[n].5).D('.j').A().l('9')}},g:3(n,a){d[n].5=a;o b=$(a).L('a').K();$(d[n].7).J(b);e.6(n);e.h(n);4(c.I)c.I.W(d[n].7,[b,a])}};2.V(3(i){o n=2.G;4(!d[n])d[n]={r:0};i=d[n].r;d[n].r++;4(i==0){c.8=$(2).S('p')||c.8;d[n].7=$('<R Q=\"P\" G=\"'+n+'\" q=\"\"'+(c.8?' p=\"p\"':'')+'>');$(2).C(d[n].7);4(c.8||c.B){}F{$(2).C($('<k y=\"m\"><a s=\"'+c.m+'\">'+c.E+'</a></k>').H(3(){e.6(n);$(2).l('9')}).v(3(){e.h(n);$(2).u('9')}).g(3(){e.g(n,2)}))}};f=$('<k y=\"j\"><a s=\"'+(2.s||2.q)+'\">'+2.q+'</a></k>');$(2).N(f);4(c.8){$(f).l('M')}F{$(f).H(3(){e.6(n);e.t(n,2)}).v(3(){e.6(n);e.h(n)}).g(3(){e.g(n,2)})};4(2.14)d[n].5=f;$(2).13();4(i+1==2.12)e.h(n)});11(n 10 d)4(d[n].5){e.t(n,d[n].5,'9');$(d[n].7).J($(d[n].5).L('a').K())}16 2}})(w);",
-    62,
-    69,
-    '||this|function|if|currentElem|drain|valueElem|readOnly|star_on||||||eStar|click|reset||star|div|addClass|cancel||var|disabled|value|count|title|fill|removeClass|mouseout|jQuery|star_hover|class|false|addBack|required|before|prevAll|cancelValue|else|name|mouseover|callback|val|text|children|star_readonly|after|siblings|hidden|type|input|attr|rating|Rating|each|apply|Cancel|fn|is|in|for|length|remove|checked|extend|return'.split(
-      '|',
-    ),
-    0,
-    {},
-  ),
-)
+  })(jQuery)
+click
