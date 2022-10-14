b128a94d0fc859766c9fb2cf210a50dd73cc27c3
Kamil <k.medrala99@gmail.com>
2022-10-14 16:23:21 +0200

    toggle querySelect one popup

--- modules/webo_shopswitch/views/js/shop-switch.js
+++ modules/webo_shopswitch/views/js/shop-switch.js
@@ -1,26 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
   const popupOpen = document.querySelectorAll("#shopswitch-toggle");
   const popupClose = document.querySelector("#shopswitch-popup-close");
+  const popupModal = document.querySelector("#shopswitch-popup");
   const popupForm = document.querySelector("#shopswitch-popup form");
   const shopChangeButton = document.querySelector("#shopswitch-popup button");
 
   popupOpen.forEach((element) => {
-    element.addEventListener("click", togglePopup);
+    element.addEventListener("click", togglePopup(popupModal));
   });
-  popupClose.addEventListener("click", togglePopup);
+  popupClose.addEventListener("click", togglePopup(popupModal));
 
   shopChangeButton.addEventListener("click", () => {
     let formData = new FormData(popupForm);
     let langPart = formData.get("language");
     let currPart = formData.get("currency");
-    togglePopup();
+    togglePopup(popupModal);
     if (langPart && currPart && prestashop) {
       window.location.href = prestashop.urls.base_url + langPart + "/?SubmitCurrency=1&id_currency=" + currPart;
     }
   });
 });
 
-function togglePopup() {
-  const popupModal = document.querySelector("#shopswitch-popup");
-  popupModal.classList.toggle("popup-hidden");
+function togglePopup(popup) {
+  popup.classList.add("popup-hidden");
 }
