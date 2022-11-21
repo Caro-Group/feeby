786cadc1496bb7dfa0738c963055733465833337
Triloworld <2671726+Triloworld@users.noreply.github.com>
2022-11-21 17:59:07 +0100

    FEEB-694 Image zoom

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1254,6 +1254,35 @@ $(document).ready(function () {
       },
     });
 
+    if ($("#main").hasClass("product-image-gallery")) {
+      $("img.js-thumb").each(function () {
+        var parent_e = $(this).parent();
+        $(this).attr("src", parent_e.data("image"));
+        $(this).data("type-zoom", parent_e.data("zoom-image"));
+      });
+
+      if ($.fn.elevateZoom !== undefined) {
+        $("img.js-thumb").elevateZoom(zoom_config);
+        //DONGND:: fix click a thumb replace all image and add fancybox
+        $("img.js-thumb").on("click", function (e) {
+          var ez = $(this).data("elevateZoom");
+          $.fancybox(ez.getGalleryList());
+          return false;
+        });
+      }
+    } else {
+      if ($.fn.elevateZoom !== undefined) {
+        $("[data-zoom-container]").elevateZoom({ gallery: "thumb-gallery" });
+
+        //pass the images to Fancybox
+        $("[data-zoom-container]").on("click", function (e) {
+          var ez = $(this).data("elevateZoom");
+          $.fancybox(ez.getGalleryList());
+          return false;
+        });
+      }
+    }
+
     handleUpdateZoom(productSwiper);
   }
 
@@ -1294,6 +1323,38 @@ $(document).ready(function () {
             },
           },
         });
+
+        if ($("#main").hasClass("product-image-gallery")) {
+          $("img.js-thumb").each(function () {
+            var parent_e = $(this).parent();
+            $(this).attr("src", parent_e.data("image"));
+            $(this).data("type-zoom", parent_e.data("zoom-image"));
+          });
+
+          if ($.fn.elevateZoom !== undefined) {
+            $("img.js-thumb").elevateZoom(zoom_config);
+            //DONGND:: fix click a thumb replace all image and add fancybox
+            $("img.js-thumb").on("click", function (e) {
+              var ez = $(this).data("elevateZoom");
+              $.fancybox(ez.getGalleryList());
+              return false;
+            });
+          }
+        } else {
+          if ($.fn.elevateZoom !== undefined) {
+            $("[data-zoom-container]").elevateZoom({
+              gallery: "thumb-gallery",
+            });
+
+            //pass the images to Fancybox
+            $("[data-zoom-container]").on("click", function (e) {
+              var ez = $(this).data("elevateZoom");
+              $.fancybox(ez.getGalleryList());
+              return false;
+            });
+          }
+        }
+
         handleUpdateZoom(productSwiper);
       }
     });
