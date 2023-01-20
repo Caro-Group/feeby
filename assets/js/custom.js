cee15e0b6a54af020188ec78f1b8941288e28e01
Triloworld <2671726+Triloworld@users.noreply.github.com>
2023-01-20 08:52:22 +0100

    Fix swiper after filter update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1527,7 +1527,7 @@ $(document).ready(function () {
   }
 
   if (typeof prestashop !== "undefined") {
-    prestashop.on("updateFacets", function (event) {
+    prestashop.on("updatedProduct", function (event) {
       if ($("[data-swiper-carousel]").length != 0) {
         new Swiper("[data-swiper-carousel]", swiperOptions);
       }
